c6410912e08699fa0bfc51207fb8afe7ae2e92f5
Aniket <aniketdayal0@gmail.com>
2023-01-02 10:47:29 +0530

    model files are added

--- model/contacts_schema.js
+++ model/contacts_schema.js
@@ -9,10 +9,10 @@ const contactSchema = new Schema({
     designation : {type : String, required : true},
     company : {type : String, required : true},
     industry : {type : String, required : true},
-    email : {type : String, required : true, unique : true},
-    phone : {type : Number, required : true, unique : true},
-    country : {type : String, required: true},
-    user : {type : ObjectId, ref : "user"}
+    email : {type : String, required : true},
+    phone : {type : Number, required : true},
+    country : {type : String, required: true}
+    // user : {type : ObjectId, ref : "user"}
 })
 
 const contacts = mongoose.model("Contact_Details", contactSchema);
